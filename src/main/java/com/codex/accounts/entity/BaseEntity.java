df4ed1918bd355a6999acffd28f1cd35eee204ec
amrbadawi1 <168075841+amrbadawi1@users.noreply.github.com>
2025-11-05 18:23:08 +0300

    audit

--- src/main/java/com/codex/accounts/entity/BaseEntity.java
+++ src/main/java/com/codex/accounts/entity/BaseEntity.java
@@ -1,37 +1,53 @@
 package com.codex.accounts.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+
+  @CreatedDate
   @Column(name = "created_at", nullable = false, updatable = false)
   private LocalDateTime createdAt;
+
+  @CreatedBy
   @Column(name = "created_by", nullable = false, updatable = false)
   private String createdBy;
+
+  @LastModifiedBy
   @Column(name = "updated_by", nullable = false)
   private String updatedBy;
+
+  @LastModifiedDate
   @Column(name = "updated_at", nullable = false)
   private LocalDateTime updatedAt;
 
-  @PrePersist
-  protected void onCreate() {
-    this.createdAt = LocalDateTime.now();
-    this.updatedAt = LocalDateTime.now();
-    this.createdBy = "system"; // حط اسم اليوزر لو عندك Security
-    this.updatedBy = "system";
-  }
-  @PreUpdate
-  protected void onUpdate() {
-    this.updatedAt = LocalDateTime.now();
-    this.updatedBy = "system";
-  }
+//  @PrePersist
+//  protected void onCreate() {
+//    this.createdAt = LocalDateTime.now();
+//    this.updatedAt = LocalDateTime.now();
+//    this.createdBy = "system";
+//    this.updatedBy = "system";
+//  }
+//
+//  @PreUpdate
+//  protected void onUpdate() {
+//    this.updatedAt = LocalDateTime.now();
+//    this.updatedBy = "system";
+//  }
 
 }
